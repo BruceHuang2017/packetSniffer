373869f1b4704343994a7b503d0898bae7104e04
BruceHub <huangyoupeng93@gmail.com>
2017-02-19 11:47:37 -0600

    Add files via upload

---
+++ ARPPacket.java
@@ -0,0 +1,4 @@
+
+public class ARPPacket extends EthernetPacket{
+
+}
---
+++ packetParser.java
@@ -0,0 +1,46 @@
+import java.nio.ByteBuffer;
+
+public class packetParser{
+  public static void main(String[] args) {
+      switch (args[0]) {
+        case "-c":
+
+          break;
+        case "-r":
+
+          break;
+        case "-o":
+
+          break;
+        case "-t":
+
+          break;
+        case "-h":
+
+          break;
+        case "-src":
+
+          break;
+        case "-dst":
+
+          break;
+        case "-sord":
+
+          break;
+        case "-sandd":
+
+          break;
+        case "-sport":
+
+          break;
+        case "-dpost":
+        
+          break;
+        default:
+          System.out.println("Illegal parameter usage.");
+          return;
+      }
+
+  }
+
+}
