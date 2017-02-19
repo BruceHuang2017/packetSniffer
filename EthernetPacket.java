0300c2723aae1f066791f644dd2bb0bd2168bf90
BruceHuang2017 <huangyoupeng93@gmail.com>
2017-02-19 14:41:32 -0600

    changes and update before go to work.

--- EthernetPacket.java
+++ EthernetPacket.java
@@ -1,4 +1,35 @@
 
 public class EthernetPacket extends Packet{
 
+// methods in this class.
+
+  public EthernetPacket(int headerLen, byte [] byteData){
+    this.header = getHeader(0, headerLen, byteData);
+    this.data = getData(0, headerLen, byteData);
+  }
+
+  public byte[] getEthernetHeader(){
+    return header;
+  }
+
+  public byte[] getEthernetData(){
+    return data;
+  }
+
+  public String getScrAddress(){
+
+  }
+
+  public String getDstAddress(){
+
+  }
+
+  public int getType(){
+
+  }
+
+// field in this class.
+
+  byte[] header;
+  byte[] data;
 }
