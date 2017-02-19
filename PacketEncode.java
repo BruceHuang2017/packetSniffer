dcf54967bf67a1d1e6061c0098eeff4f570360c9
BruceHuang2017 <huangyoupeng93@gmail.com>
2017-02-19 13:27:39 -0600

    should works now

--- PacketEncode.java
+++ PacketEncode.java
@@ -5,7 +5,7 @@ import java.nio.ByteBuffer;
  * Fetch a packet from internet and teardown to header and payload.
  * Default for ethernet frame.
  * offset = 0, headerLen = 6 + 6 + 2 + 1
- */\
+ */
 
 
 public class PacketEncode{
