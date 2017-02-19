6d22c938db2279d1ca28fdabaeacf08ead3acd37
BruceHuang2017 <huangyoupeng93@gmail.com>
2017-02-19 14:23:40 -0600

    push in new branch

---
+++ Packet.java
@@ -0,0 +1,25 @@
+import java.nio.ByteBuffer;
+
+/**
+ * Packet driver.
+ * Fetch a packet from internet and teardown to header and payload.
+ * Default for ethernet frame.
+ * offset = 0, headerLen = 6 + 6 + 2 + 1
+ */
+
+
+public class PacketEncode{
+
+  public static byte [] getHeader(int offset, int headerLen, byte [] bytes){
+
+    return header;
+  }
+
+  public static byte [] getData(){
+
+    return data;
+  }
+
+
+
+}
