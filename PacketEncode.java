5e9511e48da3e4ae134c0dc8cc625fbfa7863213
BruceHuang2017 <huangyoupeng93@gmail.com>
2017-02-19 12:23:51 -0600

    this is the first test push data by atom

---
+++ PacketEncode.java
@@ -0,0 +1,21 @@
+import java.nio.ByteBuffer;
+
+/**
+ * Packet driver.
+ * Fetch a packet from internet and teardown to header and payload.
+ * Default for ethernet frame.
+ */
+
+public class PacketEncode{
+
+  public static byte getHeader(){
+
+  }
+
+  public static byte getData(){
+
+  }
+
+
+
+}
